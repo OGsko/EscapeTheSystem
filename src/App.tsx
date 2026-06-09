4b9b6ba4f47459ee759a487cd8c46223746966fa
Isabell <isabellviolaamanda@gmail.com>
2026-06-09 12:57:08 +0200

    Set up app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage";
+import RoomPage from "./components/RoomPage/RoomPage";
+import VictoryPage from "./components/VictoryPage/VictoryPage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/rooms/:roomPath" element={<RoomPage />} />
+      <Route path="/victory" element={<VictoryPage />} />
+      <Route path="*" element={<Navigate to="/" replace/>} />
+    </Routes>
+  );
 };
 
 export default App;
